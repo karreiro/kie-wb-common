a8e64c515abd979def116f70082cba80d0f41ea5
Jozef Marko <jomarko@redhat.com>
2018-05-03 09:54:48 +0200

    JBPM-7167: Increase coverage

--- kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-project/kie-wb-common-stunner-project-api/src/test/java/org/kie/workbench/common/stunner/project/editor/impl/ProjectDiagramResourceImplTest.java
+++ kie-wb-common-stunner/kie-wb-common-stunner-extensions/kie-wb-common-stunner-project/kie-wb-common-stunner-project-api/src/test/java/org/kie/workbench/common/stunner/project/editor/impl/ProjectDiagramResourceImplTest.java
@@ -18,6 +18,7 @@ package org.kie.workbench.common.stunner.project.editor.impl;
 
 import org.junit.Test;
 import org.kie.workbench.common.stunner.core.graph.impl.GraphImpl;
+import org.kie.workbench.common.stunner.core.graph.impl.NodeImpl;
 import org.kie.workbench.common.stunner.core.graph.store.GraphNodeStoreImpl;
 import org.kie.workbench.common.stunner.project.diagram.impl.ProjectDiagramImpl;
 import org.kie.workbench.common.stunner.project.diagram.impl.ProjectMetadataImpl;
@@ -26,6 +27,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.kie.workbench.common.stunner.project.editor.ProjectDiagramResource.Type.DIAGRAM;
 import static org.kie.workbench.common.stunner.project.editor.ProjectDiagramResource.Type.XML;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 public class ProjectDiagramResourceImplTest {
 
@@ -34,6 +37,39 @@ public class ProjectDiagramResourceImplTest {
 
         final ProjectDiagramImpl projectDiagram1 = new ProjectDiagramImpl("Diagram", makeGraph(), makeMetadata());
         final ProjectDiagramImpl projectDiagram2 = new ProjectDiagramImpl("Diagram_", makeGraph(), makeMetadata());
+        final ProjectDiagramResourceImpl projectDiagramResource1 = new ProjectDiagramResourceImpl(projectDiagram1);
+        final ProjectDiagramResourceImpl projectDiagramResource2 = new ProjectDiagramResourceImpl(projectDiagram2);
+
+        assertNotEquals(projectDiagramResource1, projectDiagramResource2);
+        assertNotEquals(projectDiagramResource1.hashCode(), projectDiagramResource2.hashCode());
+    }
+
+    @Test
+    public void testEqualsWhenProjectDiagramIsDifferentGraph() {
+
+        final GraphImpl graphOne = makeGraph();
+        final GraphImpl graphTwo = makeGraph();
+        graphTwo.addNode(new NodeImpl("unique id"));
+
+        final ProjectDiagramImpl projectDiagram1 = new ProjectDiagramImpl("Diagram", graphOne, makeMetadata());
+        final ProjectDiagramImpl projectDiagram2 = new ProjectDiagramImpl("Diagram", graphTwo, makeMetadata());
+        final ProjectDiagramResourceImpl projectDiagramResource1 = new ProjectDiagramResourceImpl(projectDiagram1);
+        final ProjectDiagramResourceImpl projectDiagramResource2 = new ProjectDiagramResourceImpl(projectDiagram2);
+
+        assertNotEquals(projectDiagramResource1, projectDiagramResource2);
+        assertNotEquals(projectDiagramResource1.hashCode(), projectDiagramResource2.hashCode());
+    }
+
+    @Test
+    public void testEqualsWhenProjectDiagramIsDifferentMetadata() {
+
+        final ProjectMetadataImpl metadataOne = spy(makeMetadata());
+        final ProjectMetadataImpl metadataTwo = spy(makeMetadata());
+        doReturn("moduleOne").when(metadataOne).getModuleName();
+        doReturn("moduleTwo").when(metadataTwo).getModuleName();
+
+        final ProjectDiagramImpl projectDiagram1 = new ProjectDiagramImpl("Diagram", makeGraph(), metadataOne);
+        final ProjectDiagramImpl projectDiagram2 = new ProjectDiagramImpl("Diagram", makeGraph(), metadataTwo);
         final ProjectDiagramResourceImpl projectDiagramResource1 = new ProjectDiagramResourceImpl(projectDiagram1);
         final ProjectDiagramResourceImpl projectDiagramResource2 = new ProjectDiagramResourceImpl(projectDiagram2);
 

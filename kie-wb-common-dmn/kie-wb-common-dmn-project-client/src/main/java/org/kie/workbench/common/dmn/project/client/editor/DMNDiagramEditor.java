74ca714a4d7793e4af9992214f129c5b3c86b5d5
Guilherme Carreiro <karreiro@gmail.com>
2019-04-22 11:23:28 -0300

    Revert "DROOLS-3897: [DMN Designer] Included Models - Temporally disable Included Models (#2591)"
    This reverts commit 6c75bc7120c626b6385d3c20e7f2f6b56e794ae7.

--- kie-wb-common-dmn/kie-wb-common-dmn-project-client/src/main/java/org/kie/workbench/common/dmn/project/client/editor/DMNDiagramEditor.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-project-client/src/main/java/org/kie/workbench/common/dmn/project/client/editor/DMNDiagramEditor.java
@@ -171,7 +171,7 @@ public class DMNDiagramEditor extends AbstractProjectDiagramEditor<DMNDiagramRes
         superInitialiseKieEditorForSession(diagram);
 
         kieView.getMultiPage().addPage(dataTypesPage);
-        // TODO: {karreiro} Enable Included Models feature. # kieView.getMultiPage().addPage(includedModelsPage);
+        kieView.getMultiPage().addPage(includedModelsPage);
     }
 
     @Override

991dd7f79e1013362922be237965570a08a23fad
Guilherme Carreiro <karreiro@gmail.com>
2019-11-06 10:18:28 -0300

    .

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/widgets/codecompletion/monaco/jsenumerations/MonacoCompletionItemKind.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.client.widgets.codecompletion.monaco.jsenumerations;
+
+public class MonacoCompletionItemKind {
+
+    public static int Method = 0;
+
+    public static int Function = 1;
+
+    public static int Constructor = 2;
+
+    public static int Field = 3;
+
+    public static int Variable = 4;
+
+    public static int Class = 5;
+
+    public static int Struct = 6;
+
+    public static int Interface = 7;
+
+    public static int Module = 8;
+
+    public static int Property = 9;
+
+    public static int Event = 10;
+
+    public static int Operator = 11;
+
+    public static int Unit = 12;
+
+    public static int Value = 13;
+
+    public static int Constant = 14;
+
+    public static int Enum = 15;
+
+    public static int EnumMember = 16;
+
+    public static int Keyword = 17;
+
+    public static int Text = 18;
+
+    public static int Color = 19;
+
+    public static int File = 20;
+
+    public static int Reference = 21;
+
+    public static int Customcolor = 22;
+
+    public static int Folder = 23;
+
+    public static int TypeParameter = 24;
+
+    public static int Snippet = 25;
+}

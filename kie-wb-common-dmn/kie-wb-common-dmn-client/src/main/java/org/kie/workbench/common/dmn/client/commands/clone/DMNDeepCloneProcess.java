997efd78d41483cf0762e00fa2a202196306f68f
Guilherme Carreiro <karreiro@gmail.com>
2020-04-08 18:17:10 -0300

    Fix

--- kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/commands/clone/DMNDeepCloneProcess.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/commands/clone/DMNDeepCloneProcess.java
@@ -15,22 +15,29 @@
  */
 package org.kie.workbench.common.dmn.client.commands.clone;
 
+import java.util.AbstractMap;
+import java.util.Objects;
+import java.util.Optional;
+
 import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
 
 import elemental2.dom.DomGlobal;
-import org.apache.http.client.utils.CloneUtils;
+import org.jboss.errai.databinding.client.BindableProxy;
+import org.jboss.errai.databinding.client.BindableProxyFactory;
 import org.kie.workbench.common.stunner.core.api.FactoryManager;
 import org.kie.workbench.common.stunner.core.definition.adapter.AdapterManager;
-import org.kie.workbench.common.stunner.core.definition.clone.DeepCloneProcess;
-import org.kie.workbench.common.stunner.core.graph.Edge;
-import org.kie.workbench.common.stunner.core.graph.Node;
-import org.kie.workbench.common.stunner.core.graph.content.view.View;
+import org.kie.workbench.common.stunner.core.definition.clone.AbstractCloneProcess;
+import org.kie.workbench.common.stunner.core.definition.clone.IDeepCloneProcess;
 import org.kie.workbench.common.stunner.core.util.ClassUtils;
-import org.kie.workbench.common.stunner.core.util.DefinitionUtils;
 
 @Alternative
-public class DMNDeepCloneProcess extends DeepCloneProcess {
+public class DMNDeepCloneProcess extends AbstractCloneProcess implements IDeepCloneProcess {
+
+    private final ClassUtils classUtils;
+
+    private final AdapterManager adapterManager1;
+
     protected DMNDeepCloneProcess() {
         this(null,
              null,
@@ -39,24 +46,56 @@ public class DMNDeepCloneProcess extends DeepCloneProcess {
 
     @Inject
     public DMNDeepCloneProcess(final FactoryManager factoryManager,
-                            final AdapterManager adapterManager,
-                            final ClassUtils classUtils) {
-        super(factoryManager, adapterManager, classUtils);
+                               final AdapterManager adapterManager,
+                               final ClassUtils classUtils) {
+        super(factoryManager,
+              adapterManager);
+        this.classUtils = classUtils;
+        this.adapterManager1 = adapterManager;
     }
 
     @Override
     public <S, T> T clone(S source,
                           T target) {
-        DomGlobal.console.log("Here we are!!!");
-        final Node<View, Edge> sourceNode = (Node<View, Edge>) source;
-        final Node<View, Edge> targetNode = (Node<View, Edge>) target;
-        Object sourceDefinition = DefinitionUtils.getElementDefinition(sourceNode);
-        try {
-            Object targetDefinition = CloneUtils.clone(sourceDefinition);
-            targetNode.getContent().setDefinition(targetDefinition);
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
+        DomGlobal.console.log("=====================>> IT WORKS");
+        adapterManager1.forDefinition().getProperties(source)
+                .stream()
+                .filter(p -> !adapterManager1.forProperty().isReadOnly(p))
+                .map(p -> {
+                    String id = adapterManager1.forProperty().getId(p);
+                    Optional<?> propertyTarget = adapterManager1.forDefinition().getProperties(target)
+                            .stream()
+                            .filter(prop -> Objects.equals(adapterManager1.forProperty().getId(prop),
+                                                           id))
+                            .findFirst();
+                    return propertyTarget.isPresent() ? new AbstractMap.SimpleEntry(p,
+                                                                                    propertyTarget.get()) : null;
+                })
+                .filter(Objects::nonNull)
+                .filter(entry -> isAllowedToClone(adapterManager1.forProperty().getValue(entry.getKey())))
+                .forEach(entry -> {
+                    Object value = adapterManager1.forProperty().getValue(entry.getKey());
+                    adapterManager1.forProperty().setValue(entry.getValue(),
+                                                           cloneValue(value));
+                });
+
         return target;
+    }
+
+    private boolean isAllowedToClone(Object value) {
+        return Objects.nonNull(value) && (isSimpleValue(value) || BindableProxyFactory.isBindableType(value));
+    }
+
+    private boolean isSimpleValue(Object value) {
+        return (value instanceof String) || classUtils.isPrimitiveClass(value.getClass());
+    }
+
+    private Object cloneValue(Object value) {
+        if (value == null || isSimpleValue(value)) {
+            return value;
+        } else {
+            BindableProxy bindableProxy = (BindableProxy) BindableProxyFactory.getBindableProxy(value);
+            return bindableProxy.deepUnwrap();
+        }
     }
 }
